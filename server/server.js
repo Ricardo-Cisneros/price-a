42259326898377e1f5bb8db979dc7fef5b9fd89e
ricar <ricardocisneros197@gmail.com>
2023-05-25 15:36:08 -0700

    a

--- server/server.js
+++ server/server.js
@@ -106,7 +106,7 @@ const startApolloServer = async () => {
   server.applyMiddleware({ app });
 
   connection.once("open", () => {
-    app.listen(process.env.PORT , () => {
+    app.listen(process.env.PORT || 3001 , () => {
       console.log(`API server running on port ${PORT}!`);
       console.log(
         `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
